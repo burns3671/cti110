114db25368ad82b26467be88d4b8931c9824d775
burns3671 <43617171+burns3671@users.noreply.github.com>
2018-10-21 03:28:20 -0400

    Add files via upload

---
+++ P3HW1_RomanNumerals_burns.py
@@ -0,0 +1,31 @@
+
+# CTI-110 
+# P3HW1 - Roman Numerals 
+# omega burns
+# 10/19
+#
+
+userNumber = int(input("enter a number: "))
+
+if userNumber == 1:
+   print( "I" )
+if userNumber == 2: 
+   print( "II" )
+if userNumber == 3:
+   print( "III" )
+if userNumber == 4:
+    print( "IV" ) 
+if userNumber == 5:
+   print( "V" )
+if userNumber == 6:
+   print( "VI" )
+if userNumber == 7:
+   print( "VII" )
+if userNumber == 8:
+   print( "VIII" )
+if userNumber == 9:
+   print( "IX" )
+if userNumber == 10:
+   print( "X" )
+else:
+   print( "Error: Please enter a number between 1 and 10")
